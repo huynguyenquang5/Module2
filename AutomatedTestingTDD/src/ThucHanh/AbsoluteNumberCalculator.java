653893791e1952cfdff9445f0824988aa1e7d0d7
Quang Huy Nguyen <huynguyenquang5@gmail.com>
2022-10-17 15:38:37 +0700

    delete

--- AutomatedTestingTDD/src/ThucHanh/AbsoluteNumberCalculator.java
+++
@@ -1,9 +0,0 @@
-package ThucHanh;
-
-public class AbsoluteNumberCalculator {
-    public static int findAbsolute(int number) {
-        if(number < 0)
-            return  -number;
-        return number;
-    }
-}
