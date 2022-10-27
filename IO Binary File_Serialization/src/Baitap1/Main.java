77853ee9169fcef381eaced4d3404b17d34f307d
Quang Huy Nguyen <huynguyenquang5@gmail.com>
2022-10-27 17:21:52 +0700

    delete

--- IO Binary File_Serialization/src/Baitap1/Main.java
+++
@@ -1,8 +0,0 @@
-package Baitap1;
-
-public class Main {
-    public static void main(String[] args) {
-        ProductSystem productSystem = new ProductSystem();
-        productSystem.menuMaterialManager();
-    }
-}
